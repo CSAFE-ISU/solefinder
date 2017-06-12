c026ff2b098cb8dd29f3957f29aa9543528d5d9e
Martin Silerio-Vazquez <martin.silerio@gmail.com>
2017-06-12 17:53:08 -0500

    Just the skeleton for the registration function.

---
+++ R/regiter_images.R
@@ -0,0 +1,17 @@
+##' Register Images
+##'
+##' This function register two images based on two points
+##' selected by the user. This function is useful in shiny app.
+##' @param img_orig A matrix representing the original image
+##' @param img_trans A matrix representing the translated image
+##' @param img_orig_p1 Reference point 1 in the original image
+##' @param img_orig_p2 Reference point 2 in the original image
+##' @param img_trans_p1 Reference point 1 in the translated image
+##' @param img_trans_p2 Reference point 2 in the translated image
+##' @return The registration of the two images
+##' @author Martin Silerio-Vazquez
+##' @export
+register_images <- function (img_orig, img_trans, img_orig_p1, img_orig_p2, img_trans_p1, img_trans_p2) {
+
+    return(img_orig)
+}
