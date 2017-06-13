b6a4b5f3db65fe902c9daa0d4c79070411e8dd2f
Guillermo Basulto <guillermobasulto@gmail.com>
2017-06-13 11:29:41 -0500

    Add Lena documentation.

--- R/lena.R
+++ R/lena.R
@@ -5,3 +5,35 @@
 #' @format A 512x512 matrix
 #' @source \url{http://www.diamondse.info/}
 "lena"
+
+#' Boots
+#'
+#' Sample scans of boots
+#'
+#' These images have a very low resolution (compared to the images
+#' that this packages is intended to handle) due to the reason that
+#' they will be included in the package.
+#'
+#' @format 511x198 matrices representing scans from two different
+#'     boots.
+#' @examples
+#' dim(boot1)
+#' dim(boot2)
+#' image(boot1)
+"boot1"
+
+#' Boots
+#'
+#' Sample scans of boots
+#'
+#' These images have a very low resolution (compared to the images
+#' that this packages is intended to handle) due to the reason that
+#' they will be included in the package.
+#'
+#' @format 511x198 matrices representing scans from two different
+#'     boots.
+#' @examples
+#' dim(boot1)
+#' dim(boot2)
+#' image(boot1)
+"boot2"
