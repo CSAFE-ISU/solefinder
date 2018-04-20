dd40115599e3045932c5e97467e6a02af99ad433
Martin Silerio-Vazquez <martin.silerio@gmail.com>
2018-04-20 18:05:10 -0500

    Building function to preprocess.

---
+++ vignettes/preprocess.R
@@ -0,0 +1,19 @@
+## Here we will crop, invert the intensity of the pixels and apply a threshold to a given set of images
+## We will read them from the folder IN
+## And we will write them in the folder OUT
+## The list of names to be processed will be read from a textfile
+
+preProcessTIFF <- function(fileList) {
+
+  n < length(fileList)
+
+  for(i in 1:n) {
+    fileName <- fileList[i]
+    img <- readImage(paste0("\\IN","\\",fileName))
+    img_gray <- channel(img, mode = "gray")
+    img_cropped <- img1_gray[161:1805, 161:4403]
+    img_cropped_neg <- max(img_cropped) - img_cropped
+    writeImage(img_cropped_neg, paste0("\\OUT","\\",fileName), compression = "none", bits.per.sample = 8)
+  }
+
+}
